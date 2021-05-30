8ad8ac72f46caf8b8577a1fd6a59e6da437aa0b7
JacksonYu92 <64330362+JacksonYu92@users.noreply.github.com>
2021-05-30 15:41:52 -0500

    Add files via upload
    Update form request for contact page

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
+My_email = os.environ["EMAIL"]
+password = os.environ["PASSWORD"]
 
 posts=requests.get("https://api.npoint.io/0067e63917ca7a5034d9").json()
 
@@ -14,9 +18,36 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/contact')
+@app.route('/contact', methods=['POST','GET'])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone_number = request.form["phone number"]
+        message = request.form["message"]
+        email_msg = f"name: {name}\nemail: {email}\nphone number: {phone_number}\nmessage: {message}"
+        title = "Successfully sent you message"
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=My_email, password=password)
+            connection.sendmail(
+                from_addr=My_email,
+                to_addrs=My_email,
+                msg=f"Subject:New Message\n\n{email_msg}"
+            )
+    else:
+        title = "Contact Me"
+    return render_template("contact.html",title=title)
+
+# @app.route('/form-entry', methods=["POST"])
+# def receive_data():
+#     name = request.form["name"]
+#     email = request.form["email"]
+#     phone_number = request.form["phone number"]
+#     message = request.form["message"]
+#     print(f"name: {name}\nemail: {email}\nphone number: {phone_number}\nmessage: {message}")
+#     return "<h1>Successfully sent you message</h1>"
+
 
 @app.route('/post/<int:id>')
 def post(id):
@@ -28,3 +59,4 @@ def post(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
